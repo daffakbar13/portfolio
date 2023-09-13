c19b9da963fe62182e00464611cd28128d71ea31
m.daffa.akbar <=>
2023-09-13 18:55:52 +0700

    feat: tmp folder

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,4 +1,4 @@
-import { readFile, writeFile } from 'fs/promises'
+import { mkdir, readFile, writeFile } from 'fs/promises'
 import { NextRequest, NextResponse } from 'next/server'
 import path from 'path'
 
@@ -7,7 +7,17 @@ type ResponseData = {
 }
 
 export async function GET(req: NextRequest) {
-  const filePath = path.join(process.cwd(), '/public/json/logs.json')
+  function getFilePath() {
+    const fileName = '/logs.json'
+    const directory = '/tmp/json'
+    if (process.env.NODE_ENV === 'development') {
+      mkdir(path.join(process.cwd(), directory), { recursive: true })
+      return path.join(process.cwd(), directory, fileName)
+    }
+    mkdir(directory, { recursive: true })
+    return directory + fileName
+  }
+  const filePath = getFilePath()
   await writeFile(filePath, JSON.stringify({ message: req.nextUrl.searchParams.get('text') }))
   const data = await readFile(filePath, { encoding: 'utf8' })
   return NextResponse.json<ResponseData>({ ...JSON.parse(data), ...{ filePath } }, { status: 200 })
